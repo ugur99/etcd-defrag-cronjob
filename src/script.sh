c66e42261462ad00cb8a76687be95e8c8a6b0e29
Ugur Can Ozturk <57688057+ugur99@users.noreply.github.com>
2022-12-15 08:58:27 +0300

    [fix]: script.sh
    Signed-off-by: Ugur Can Ozturk <57688057+ugur99@users.noreply.github.com>

--- src/script.sh
+++ src/script.sh
@@ -16,20 +16,19 @@ if [[ $(curl -s $PROMETHEUSSVC.$PROMETHEUSNS:9090/api/v1/alerts |  jq  '.data.al
     for i in $(curl -s --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/api/v1/namespaces/kube-system/endpoints/etcd  | jq  '.subsets[].addresses[].ip' | cut -d'"' -f 2);do 
     # Check if i is not the leader then run etcdctl defrag command
       if [ $(etcdctl --endpoints=https://$i:2379 endpoint status --key=$ETCDCTL_KEY --cert=$ETCDCTL_CERT --cacert=$ETCDCTL_CACERT  |awk '{print $6}'| cut -f 1 -d  ",") == "false" ]; then
-        # Its not a leader, letd defrag it!
+        # It's not a leader, let's defrag it!
         etcdctl --endpoints=https://$i:2379 defrag --key=$ETCDCTL_KEY --cert=$ETCDCTL_CERT --cacert=$ETCDCTL_CACERT >> /proc/1/fd/1
-        sleep 10
       else
-        # Its a leader, lets skip it for a now.
+        # It's a leader, let's skip it for a now.
         :
       fi
     done
 
     for i in $(curl -s --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/api/v1/namespaces/kube-system/endpoints/etcd  | jq  '.subsets[].addresses[].ip' | cut -d'"' -f 2);do
-    # Check if i is leader then run etcdctl defrag command
+    # Check if it's a leader then run etcdctl defrag command
     echo "Starting Defrag from the etcd leader node" >> /proc/1/fd/1
       if [ $(etcdctl --endpoints=https://$i:2379  endpoint status --key=$ETCDCTL_KEY --cert=$ETCDCTL_CERT --cacert=$ETCDCTL_CACERT  |awk '{print $6}'| cut -f 1 -d  ",") = "true" ]; then
-       # Its a leader, lets defrag it!
+       # Its a leader, let's defrag it!
        etcdctl --endpoints=https://$i:2379  defrag --key=$ETCDCTL_KEY --cert=$ETCDCTL_CERT --cacert=$ETCDCTL_CACERT >> /proc/1/fd/1
       else
       :
